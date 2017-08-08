57102bf557113af1b044fe9a5a05d35edde4ebb1
Grey <GreyLee369@gmail.com>
2017-08-08 09:43:00 -0400

    "udp listener"

--- src/thread/UdpHandler1.java
+++ src/thread/UdpHandler1.java
@@ -144,7 +144,8 @@ public class UdpHandler1 extends Thread {
     public void sentMessageForReply(String content) {
 
         try {
-            byte[] message = content.getBytes();
+            byte[] message = new byte[2000]; 
+            message = content.getBytes();
             DatagramPacket replyPacket = new DatagramPacket(message, message.length, datagramPacket.getAddress(), datagramPacket.getPort());
             myDatagramSocket.send(replyPacket);
             System.out.println("xxxxxxx");//todo
